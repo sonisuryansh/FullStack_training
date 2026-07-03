096d91f223fa79bc5ffcb7389455c46c8445b26a
Suryansh Soni <146843858+sonisuryansh@users.noreply.github.com>
2026-07-03 10:35:54 +0530

    Checking the number has how much days into that month

--- Frontend2/Question_Solution.js
+++ Frontend2/Question_Solution.js
@@ -42,3 +42,18 @@
                     alert("Invalid day number. Please enter a number between 1 and 7.");
             }
         }
+
+        // Checking the Number of days in a month
+
+        function monthDays(){
+            var monthNum = parseInt(prompt("Enter the number of the month (1-12):")); 
+            if(monthNum == 1 || monthNum == 3 || monthNum == 5 || monthNum == 7 || monthNum == 8 || monthNum == 10 || monthNum == 12){
+                alert("The month has 31 days.");
+            }else if(monthNum == 4 || monthNum == 6 || monthNum == 9 || monthNum == 11){
+                alert("The month has 30 days.");
+            }else if(monthNum == 2){
+                alert("The month has 28 days or 29 days in a leap year.");
+            }else{
+                alert("Invalid month number. Please enter a number between 1 and 12.");
+            }
+        }
